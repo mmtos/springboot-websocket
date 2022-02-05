ef168fae0f0882011062ccb9fb64a392250b6c9b
mmtos <js223kr92@naver.com>
2022-02-05 17:01:06 +0900

    handshake인터셉터 추가

--- src/main/java/com/example/ws/config/WebSocketConfig.java
+++ src/main/java/com/example/ws/config/WebSocketConfig.java
@@ -1,32 +1,51 @@
 package com.example.ws.config;
 
 import com.example.ws.handler.EchoWebSocketHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.HandshakeInterceptor;
 
 import java.net.http.WebSocket;
+import java.util.Map;
 
 @Configuration
 @EnableWebSocket
+@Slf4j
 public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(myHandler(), "/ws/echo")
                 //CORS 설정
-                .setAllowedOrigins("*");
+                .setAllowedOrigins("*")
+                .addInterceptors(new MyHandshakeInterceptor());
                 //.withSockJS();
-                // default taskScheduler사용
-                //.setTaskScheduler(myScheduler());
     }
 
     @Bean
     public WebSocketHandler myHandler(){
         return new EchoWebSocketHandler();
+    }
+
+    class MyHandshakeInterceptor implements HandshakeInterceptor{
+
+        @Override
+        public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+            log.info("MyHandshakeInterceptor.beforeHandshake()");
+            return true;
+        }
+
+        @Override
+        public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+            log.info("MyHandshakeInterceptor.afterHandshake() 작동");
+        }
     }
 }
